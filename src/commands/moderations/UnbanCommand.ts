9dcd88734553b95c1cc77ff7bb2b142ebef043ed
Cool-Bread <129181975+Cool-Bread@users.noreply.github.com>
2024-03-28 23:06:58 +0100

    Optimize UnbanCommand.ts
    Grouped interaction options fetching for better readability.
    
    Replaced direct color string values with uppercase equivalents for consistency.
    
    Adjusted indentation for better readability.
    
    Fixed typo in the "user is not banned" message.
    
    Renamed variables to start with lowercase letters for consistency.
    
    Signed-off-by: Cool-Bread <129181975+Cool-Bread@users.noreply.github.com>

--- src/commands/moderations/UnbanCommand.ts
+++ src/commands/moderations/UnbanCommand.ts
@@ -6,46 +6,43 @@ export default class UnbanCommand extends BaseCommand {
 	constructor() {
 		super("unban", "moderation");
 	}
+
 	async run(client: DiscordClient, interaction: CommandInteraction) {
-		const bannedId = interaction.options.data[0].value as string;
-		const reason =
-			(interaction.options.get("reason", false)?.value as string) ||
-			"No Reason Provided";
+		const options = interaction.options.data;
+		const bannedId = options[0].value as string;
+		const reason = (interaction.options.get("reason", false)?.value as string) || "No Reason Provided";
+
 		const banList = await interaction.guild!.bans.fetch();
 		const bannedUser = banList.get(bannedId);
+
 		if (!bannedUser) {
 			const embed = new EmbedBuilder()
-				.setColor("Red")
-				.setDescription(`❗ This is user is not banned`);
+				.setColor("RED")
+				.setDescription(`❗ This user is not banned`);
 			await interaction.followUp({ embeds: [embed] });
 			return;
 		}
+
 		await interaction.guild!.bans.remove(bannedId, reason);
+
 		const channelEmbed = new EmbedBuilder()
-			.setColor("Green")
+			.setColor("GREEN")
 			.setDescription(`✅ **${bannedUser.user.tag} Unbanned**`);
 		await interaction.followUp({ embeds: [channelEmbed] });
+
 		const logEmbed = new EmbedBuilder()
 			.setAuthor({
 				name: `Moderation | Unban | ${bannedUser.user.tag}`,
 				iconURL: bannedUser.user.displayAvatarURL(),
 			})
 			.addFields([
-				{
-					name: "User",
-					value: `<@${bannedUser.user.id}>`,
-					inline: true,
-				},
-				{
-					name: "Moderator",
-					value: `<@${interaction.member?.user.id}>`,
-					inline: true,
-				},
+				{ name: "User", value: `<@${bannedUser.user.id}>`, inline: true },
+				{ name: "Moderator", value: `<@${interaction.member?.user.id}>`, inline: true },
 				{ name: "Reason", value: reason, inline: true },
 			])
 			.setFooter({ text: bannedUser.user.id })
 			.setTimestamp()
-			.setColor("#7289da");
+			.setColor("#7289DA");
 		await client.logChannel.send({ embeds: [logEmbed] });
 	}
 }
